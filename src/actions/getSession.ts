c8a65a534a2c2fdd9124e480cf2dbe9f40ab3972
Devsethi3 <devsethi4522@gmail.com>
2024-03-28 15:11:28 +0530

    final commit

--- src/actions/getSession.ts
+++ src/actions/getSession.ts
@@ -1,4 +1,4 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 
 export default async function getSession() {
--- src/pages/api/pusher/auth.ts
+++ src/pages/api/pusher/auth.ts
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
-import { authOptions } from "@/app/api/auth/[...nextauth]/auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { pusherServer } from "@/lib/pusher";
 
 export default async function handler(
