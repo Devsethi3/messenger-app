6fd6d78d14f7f43b0bd0618bce035f7ee322c1b7
Devsethi3 <devsethi4522@gmail.com>
2024-03-28 14:55:07 +0530

    final commit

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -59,6 +59,4 @@ export const authOptions: AuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
 };
 
-const handler = NextAuth(authOptions);
-
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
